0c3cfc284ec9edc85202f06c5f6b9b2d1144ea60
Tomi Chen <tctree333@users.noreply.github.com>
2021-07-25 12:30:11 -0700

    formatting

--- screens/code.js
+++ screens/code.js
@@ -3,7 +3,7 @@ import {SafeAreaView, Text} from 'react-native';
 import {WebView} from 'react-native-webview';
 import codemirrorHtml from '../assets/codeScreen/html.js';
 import codeMirrorJs from '../assets/codeScreen/dist/editor.bundle.js';
-import {readFile} from "../assets/FileSystem";
+import {readFile} from '../assets/FileSystem';
 
 const DEFAULT_CONTENT = `import React from 'react';
 import { Text, View } from 'react-native';
@@ -25,66 +25,74 @@ export default HelloWorldApp;`;
 // const PREVENT_ZOOM = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); \``;
 
 const App = () => {
-    const file = 'project_1/test.txt';
+  const file = 'project_1/test.txt';
 
-    const [loading, setLoading] = useState(true);
-    const [initWebViewParams, setInitWebViewParams] = useState({});
-    useEffect(() => {
-        readFile(file).then(({data, meta}) => {
-            console.log(meta);
-            setInitWebViewParams({
-                initialString: JSON.stringify(data),
-                fileName: JSON.stringify(meta.name),
-                projectName: JSON.stringify(meta.projectName)
-            })
-            setLoading(false);
-            // console.log(initWebViewParams);
-        });
-    }, [])
+  const [loading, setLoading] = useState(true);
+  const [initWebViewParams, setInitWebViewParams] = useState({});
+  useEffect(() => {
+    readFile(file).then(({data, meta}) => {
+      console.log(meta);
+      setInitWebViewParams({
+        initialString: JSON.stringify(data),
+        fileName: JSON.stringify(meta.name),
+        projectName: JSON.stringify(meta.projectName),
+      });
+      setLoading(false);
+      // console.log(initWebViewParams);
+    });
+  }, []);
 
-    const initializeContentJs = `
-  editor.initializeContent(${initWebViewParams.initialString || DEFAULT_CONTENT});
-  document.getElementById('filename').innerText = ${initWebViewParams.fileName || 'unknown'};
-  document.getElementById('projectName').innerText = ${initWebViewParams.projectName ||'Unkown'};
+  const initializeContentJs = `
+  editor.initializeContent(${
+    initWebViewParams.initialString || DEFAULT_CONTENT
+  });
+  document.getElementById('filename').innerText = ${
+    initWebViewParams.fileName || 'unknown'
+  };
+  document.getElementById('projectName').innerText = ${
+    initWebViewParams.projectName || 'Unkown'
+  };
   `;
-    const initialInjectedJs = `
+  const initialInjectedJs = `
       ${codeMirrorJs};
       ${initializeContentJs}
   `;
-    console.log(initialInjectedJs);
+  console.log(initialInjectedJs);
 
-    const fetchContentJs = `
+  const fetchContentJs = `
     editor.fetchContent();
   `;
 
-    const webview = React.useRef(null);
+  const webview = React.useRef(null);
 
-    useEffect(() => {
-        setTimeout(() => {
-            webview.current.injectJavaScript(fetchContentJs);
-        }, 5000);
-    }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      webview.current.injectJavaScript(fetchContentJs);
+    }, 5000);
+  }, []);
 
-    return (
-        <SafeAreaView>
-            {loading && <Text>Loading...</Text>}
-            {!loading && <WebView
-                ref={webview}
-                originWhitelist={['*']}
-                source={{html: codemirrorHtml}}
-                injectedJavaScript={initialInjectedJs}
-                onMessage={event => {
-                    console.log(event.nativeEvent.data);
-                }}
-                containerStyle={{minHeight: '100%', minWidth: '100%'}}
-                onNavigationStateChange={newNavState => {
-                    console.log({newNavState});
+  return (
+    <SafeAreaView>
+      {loading && <Text>Loading...</Text>}
+      {!loading && (
+        <WebView
+          ref={webview}
+          originWhitelist={['*']}
+          source={{html: codemirrorHtml}}
+          injectedJavaScript={initialInjectedJs}
+          onMessage={event => {
+            console.log(event.nativeEvent.data);
+          }}
+          containerStyle={{minHeight: '100%', minWidth: '100%'}}
+          onNavigationStateChange={newNavState => {
+            console.log({newNavState});
 
-                    webview.current.stopLoading();
-                }}
-            />}
-        </SafeAreaView>
-    );
+            webview.current.stopLoading();
+          }}
+        />
+      )}
+    </SafeAreaView>
+  );
 };
 
 export default App;
