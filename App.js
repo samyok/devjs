faf9d70a7229cb67e5fcc78e84dfacfa5087f69a
Weilin Hu <Weilinhu@wharton.upenn.edu>
2021-07-25 08:44:06 -0500

    cleanup

--- App.js
+++ App.js
@@ -5,53 +5,15 @@
  * @format
  * @flow strict-local
  */
-import nodejs from 'nodejs-mobile-react-native';
-
-import type {Node} from "react";
 import React, {useEffect} from "react";
-import RNBootSplash from "react-native-bootsplash";
-
-import {SafeAreaView, StyleSheet, Text, useColorScheme, View} from "react-native";
-
-import {Colors,} from "react-native/Libraries/NewAppScreen";
-
 import Projects from './screens/Projects';
 
+import type {Node} from "react";
+import nodejs from 'nodejs-mobile-react-native';
+import RNBootSplash from "react-native-bootsplash";
 
-const Section = ({children, title}): Node => {
-    const isDarkMode = useColorScheme() === "dark";
-
-
-    return (
-        <View style={styles.sectionContainer}>
-            <Text
-                style={[
-                    styles.sectionTitle,
-                    {
-                        color: isDarkMode ? Colors.white : Colors.black,
-                    },
-                ]}>
-                {title}
-            </Text>
-            <Text
-                style={[
-                    styles.sectionDescription,
-                    {
-                        color: isDarkMode ? Colors.light : Colors.dark,
-                    },
-                ]}>
-                {children}
-            </Text>
-        </View>
-    );
-};
 
 const App: () => Node = () => {
-    const isDarkMode = useColorScheme() === "dark";
-
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
     useEffect(() => {
         setTimeout(() => {
             RNBootSplash.hide({fade: true});
@@ -70,23 +32,5 @@ const App: () => Node = () => {
     );
 };
 
-const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: "600",
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: "400",
-    },
-    highlight: {
-        fontWeight: "700",
-    },
-});
 
 export default App;
